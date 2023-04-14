21230037c66b1db44ac43b6b2ba6fc27619f3f5f
jmartinezac <jim_2306@hotmail.com>
2023-04-14 12:20:25 -0500

    agragacion de opcion de busqueda pokemon y parte del navbar

--- src/js/apipoke.js
+++ src/js/apipoke.js
@@ -1,5 +1,6 @@
 const cardNombre = document.getElementById("card-nombre");
 let bodyCards = document.getElementById("listaTodos");
+const buttonBuscar = document.getElementById("buttonBuscar");
 
 const url = `https://pokeapi.co/api/v2/pokemon/`;
 
@@ -7,9 +8,7 @@ consultarPokemon();
 
 async function consultarPokemon() {
   //const namePokemon = document.getElementById("pokemon-name").value;
-
-  //debugger;
-  for (let i = 0; i < 600; i++) {
+  for (let i = 1; i < 200; i++) {
     const response = await fetch(`${url}${i}`);
     //console.log(response);
     // Si la consulta fue exitosa, convertir la respuesta a JSON
@@ -19,20 +18,40 @@ async function consultarPokemon() {
       mostrarPokemon(pokemon);
     }
   }
+}
 
-  function mostrarPokemon(pokemon) {
-    let tiposs = pokemon.types.map(
-      (type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`
-    );
-    tiposs = tiposs.join("");
-    console.log(tiposs);
-    console.log(pokemon);
-    const sprite = pokemon.sprites.other;
-    console.log(sprite);
-    const { dream_world, home } = sprite;
-    const div = document.createElement("div");
-    div.classList.add("cardPokemon");
-    div.innerHTML = `
+buttonBuscar.addEventListener("click", (event) => {
+  bodyCards.innerHTML = "";
+  event.preventDefault(event);
+  const BuscarPokemon = document.getElementById("inputBuscar").value;
+  console.log(BuscarPokemon);
+  if (BuscarPokemon.length === 0) {
+    consultarPokemon();
+    //window.location.reload();
+  } else {
+    console.log(BuscarPokemon);
+    fetch(`${url}${BuscarPokemon.toLowerCase()}`)
+      .then((response) => response.json())
+      .then((pokemon) => {
+        console.log(pokemon);
+        mostrarPokemon(pokemon);
+      });
+  }
+});
+
+function mostrarPokemon(pokemon) {
+  let tiposs = pokemon.types.map(
+    (type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`
+  );
+  tiposs = tiposs.join("");
+  //console.log(tiposs);
+  //console.log(pokemon);
+  const sprite = pokemon.sprites.other;
+  //console.log(sprite);
+  const { dream_world, home } = sprite;
+  const div = document.createElement("div");
+  div.classList.add("cardPokemon");
+  div.innerHTML = `
     <div class="pokemon">
     <p class="pokemonIdBig">#${pokemon.id}</p>
     <div class="pokemonImagen">
@@ -50,7 +69,6 @@ async function consultarPokemon() {
       </div>
     </div>
     </div>`;
-    //const { stast, types } = pokemon;
-    bodyCards.appendChild(div);
-  }
+  //const { stast, types } = pokemon;
+  bodyCards.appendChild(div);
 }
